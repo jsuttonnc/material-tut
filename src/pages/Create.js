8d8d075d6fea6a2eec9ebb985447e4dcbcac23ac
jsuttonnc <jsuttonnc@icloud.com>
2021-04-12 15:13:29 -0400

    Project Started

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -41,13 +41,17 @@ export default function Create() {
       setDetailsError(true)
     }
     if (title && details) {
-      fetch('http://localhost:8000/notes', {
+      fetch('https://4pavetg9a3.execute-api.us-east-1.amazonaws.com/items', {
         method: 'POST',
         headers: {"Content-type": "application/json"},
         body: JSON.stringify({ title, details, category })
       }).then(() => history.push('/'))
     }
   }
+
+  const style = {
+    cursor: 'pointer'
+  };
 
   return (
     <Container>
@@ -98,6 +102,7 @@ export default function Create() {
           type="submit" 
           color="secondary" 
           variant="contained"
+          style={style}
           endIcon={<KeyboardArrowRight />}
         >
           Submit
--- src/pages/Notes.js
+++ src/pages/Notes.js
@@ -7,14 +7,58 @@ import Masonry from "react-masonry-css";
 export default function Notes() {
   const [notes, setNotes] = useState([]);
 
-  useEffect(() => {
-    fetch("http://localhost:8000/notes")
-      .then((res) => res.json())
-      .then((data) => setNotes(data));
+    useEffect(() => {
+    fetch("https://4pavetg9a3.execute-api.us-east-1.amazonaws.com/items")
+    .then(response => response.body)
+    .then(rb => {
+      const reader = rb.getReader();
+    
+      return new ReadableStream({
+        start(controller) {
+          // The following function handles each data chunk
+          function push() {
+            // "done" is a Boolean and value a "Uint8Array"
+            reader.read().then( ({done, value}) => {
+              // If there is no more data to read
+              if (done) {
+                controller.close();
+                return;
+              }
+              // Get the data and send it to the browser via the controller
+              controller.enqueue(value);
+              push();
+            })
+          }
+          push();
+        }
+      });
+    })
+    .then(stream => {
+      // Respond with our stream
+      return new Response(stream, { headers: { "Content-Type": "text/html" } }).text();
+    })
+    .then(result => {
+      // Do things with result
+      var obj = JSON.parse(result);
+      setNotes(obj.Items);
+    });
+  
   }, []);
 
+/*   useEffect(() => {
+    fetch("https://4pavetg9a3.execute-api.us-east-1.amazonaws.com/items")
+      .then((res) => {
+          res.json()
+          console.log(res);
+      })
+      .then((data) => {
+        setNotes(data)
+      });
+  
+  }, []); */
+
   const handleDelete = async (id) => {
-    await fetch("http://localhost:8000/notes/" + id, {
+    await fetch("https://4pavetg9a3.execute-api.us-east-1.amazonaws.com/items/" + id, {
       method: "DELETE",
     });
 
